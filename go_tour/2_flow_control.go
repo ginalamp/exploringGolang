3f445d3760f4483aaaffb61165f2071e9c370fd4
ginalamp <gina.n.lamprecht@gmail.com>
2021-09-28 19:55:30 +0200

    switch statements

--- go_tour/2_flow_control.go
+++ go_tour/2_flow_control.go
@@ -1,8 +1,12 @@
+// basic flow control statements: for, if, else, switch, defer
+
 package main
 
 import (
 	"fmt"
 	"math"
+	"runtime"
+	"time"
 )
 
 // basic for loop understanding
@@ -80,9 +84,55 @@ func pow(x, n, lim float64) float64 {
 	return lim
 }
 
+// basic switch understanding
+func switch_statements() {
+	fmt.Println("Go runs on ")
+
+	// switch cases do not need to be integers
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+		// do not need break statement (automatically added)
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
+
+	// switch cases do not need to be constants
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+
+	switch time.Saturday {
+	case today:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+
+	}
+
+	// switch with no conditions is the same as switch true (ideal for long if-then-else chains)
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning.")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
+}
+
 func main() {
 	fmt.Println("Flow control statements: for, if, else, switch, defer")
-	for_loops()
+	// for_loops()
 	// if_else()
+	switch_statements()
 
 }
